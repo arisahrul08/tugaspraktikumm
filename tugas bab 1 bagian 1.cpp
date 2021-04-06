88cdfa50e4c48fae17051483b02f5008b514df93
ari sahrul08 <81893214+arisahrul08@users.noreply.github.com>
2021-04-06 11:39:19 +0700

    Add files via upload

---
+++ tugas bab 1 bagian 1.cpp
@@ -0,0 +1,25 @@
+//nama: ARI SAHRUL
+//npm:20.14.1.0007
+//nama prgm: biodata singkat bab 1 bagian 1.pas
+
+
+
+
+ #include <iostream>
+ 
+ using namespace std;
+ 	 
+ 	 int main()
+ 	 
+ 	 {
+ 	 	std::cout <<" nama mahasiswa:"<<" ARI SAHRUL"<<std::endl;
+ 	 	std::cout <<" npm : "<<" 20.14.1.0007"<<std::endl;
+ 	 	std::cout <<" Alamat : "<<" blok sekadi, rt/rw/ 01/01 kec gunung jati, kab cirebon"<<std::endl;
+ 	 	std::cout <<" TTL  : "<<" cirebon 8 okto 2000"<<std::endl;
+ 	 	std::cout <<" no Hp : "<<"081939706843"<<std::endl;
+ 	 	std::cout <<" motto  : "<< "kalau mao sukses harus cape"<<std::endl;
+ 	 	
+ 	 	
+ 	 	
+ 	 	
+	  }
